adc13dec292ae48b23103ccb2d822e5960748885
puneet2121 <bedipuneet21@gmail.com>
2023-01-08 20:36:36 -0700

    First commit

---
+++ 02-tours/setup/src/App.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-tours-project'
+function App() {
+  
+  const [loading,setLoading] = useState(true);
+  const [tours,setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true)
+    try {
+      const data = await fetch(url);
+      const tour = await data.json();
+      setLoading(false);
+      setTours(tour);
+
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+   
+  }
+  useEffect(() => {
+    fetchTours();
+  },[])
+  if(loading) {
+    return <h2><Loading /></h2>
+  }
+  return (
+    <main>
+    < Tours tours={tours}/>
+    </main> 
+  )
+}
+
+export default App
---
+++ 02-tours/setup/src/Tour.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+
+const Tour = (props) => {
+  return <article className='single-tour'>
+  <img src={props.image} alt="" /></article>;
+};
+
+export default Tour;
