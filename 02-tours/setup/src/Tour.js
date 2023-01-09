77eab73f57f8d75924d2d1a23c9b4274ffb08f19
puneet2121 <bedipuneet21@gmail.com>
2023-01-08 22:20:12 -0700

    add images price info and button to the tour

--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -2,7 +2,16 @@ import React, { useState } from 'react';
 
 const Tour = (props) => {
   return <article className='single-tour'>
-  <img src={props.image} alt="" /></article>;
+  <img src={props.image} alt={props.name} />
+  <footer>
+    <div className='tour-info'>
+      <h4>{props.name}</h4>
+      <h4 className='tour-price'>{props.price}</h4>
+      
+    </div>
+    <p>{props.info}</p>
+    <button className='delete-btn'>Not interested</button>
+    </footer></article>;
 };
 
 export default Tour;
