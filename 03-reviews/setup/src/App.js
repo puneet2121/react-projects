619ec1acb0a6a7d8bb3821389dbacd203188ff80
puneet2121 <bedipuneet21@gmail.com>
2023-01-11 22:49:49 -0700

    add state to the project

--- 03-reviews/setup/src/App.js
+++ 03-reviews/setup/src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import Review from './Review';
-import { DiApple } from 'react-icons/di';
+import React from "react";
+import Review from "./Review";
 function App() {
-  return <h2>reviews project setup <DiApple /></h2>;
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>reviews project setup</h2>
+          <div className="underline"></div>
+          </div>
+          <Review />
+      </section>
+    </main>
+  );
 }
 
 export default App;
