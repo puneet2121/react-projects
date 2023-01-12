84d482f18979f131955981626c555428c2901bd9
puneet2121 <bedipuneet21@gmail.com>
2023-01-11 22:55:30 -0700

    add next and prev functions

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -5,6 +5,19 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
+
+  const prev = () => {
+    setIndex((index) => {
+      let newValue = index - 1;
+      return newValue;
+    });
+  }
+  const next = () => {
+    setIndex((index) => {
+      let newValue = index + 1;
+      return newValue;
+    });
+  }
   return (
     <article className="review">
       <div className="img-container">
@@ -17,14 +30,14 @@ const Review = () => {
       <p className="job">{job}</p>
       <p className="info">{text}</p>
       <div className="button-container">
-        <button className="prev-btn">
+        <button className="prev-btn" onClick={prev}>
           <FaChevronLeft />
         </button>
-        <button className="next-btn">
+        <button className="next-btn" onClick={next}>
           <FaChevronRight />
         </button>
       </div>
-      <button className="random-btn">Random</button>
+      <button className="random-btn" >Random</button>
     </article>
   );
 };
