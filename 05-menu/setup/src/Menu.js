b82826781728a1b99766903f5f7b852fe388b4d1
puneet2121 <bedipuneet21@gmail.com>
2023-01-17 22:43:28 -0700

    add price and desc

--- 05-menu/setup/src/Menu.js
+++ 05-menu/setup/src/Menu.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React from "react";
 
 const Menu = (props) => {
-  return <div className='section-center'>
-    {props.items.map((menuItem) => {
-      const {id, title, img, desc} = menuItem;
-      return <article key={id} className="menu-item">
-        <img className="photo" src={img} alt={title} />
-      </article>
-    })} 
-  </div>
+  return (
+    <div className="section-center">
+      {props.items.map((menuItem) => {
+        const { id, title, img, desc, price } = menuItem;
+        return (
+          <article key={id} className="menu-item">
+            <img className="photo" src={img} alt={title} />
+            <div className="item-info">
+              <header>
+                <h4>{title}</h4>
+                <h4 className="price">${price}</h4>
+              </header>
+              <p className="item-text">{desc}</p>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Menu;
