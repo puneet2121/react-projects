f3c164863efee3efb0ccac2f3263d511e3870e72
puneet2121 <bedipuneet21@gmail.com>
2023-01-22 23:49:45 -0700

    add use state and effect to fetch data

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -4,6 +4,25 @@ import { FaAngleDoubleRight } from 'react-icons/fa'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
+  const [loading,setLoading] = useState(true);
+  const [jobs,setJobs] = useState([]);
+  const [value,setValue] = useState(0);
+  const fetchJobs = async () => {
+    const result = await fetch(url);
+    const newJobs = await result.json();
+    setJobs(newJobs);
+    setLoading(false);
+  }
+  useEffect(() => {
+    fetchJobs();
+  },[])
+  if (loading) {
+    return (
+      <section className='section loading'>
+        <h1>loading.</h1>
+      </section>
+    )
+  }
   return <h2>tabs project setup</h2>
 }
 
